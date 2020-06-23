c5aa9e3c0b42d15c6339928aaf1ab8def7628856
LouiseBalbi <67003682+LouiseBalbi@users.noreply.github.com>
2020-06-23 13:19:22 +0200

    First commit

---
+++ src/fr/declaration/variable/DeclarationApp.java
@@ -0,0 +1,20 @@
+package fr.declaration.variable;
+
+public class DeclarationApp {
+
+	public static void main(String[] args) {
+		byte number1 = 2;
+		short number2 = 3;
+		int nunber3 = 4;
+		long number4 = 100000L;
+		float number5 = 3.45f;
+		double number6 = 5.1234;
+		char test = 'a';
+		boolean a = false;
+		String test2 = "Bonjour";
+		System.out.println(number1);
+		String randomString = "Voici le résultat d’un calcul : \n1+5=6";
+		System.out.println(randomString);
+	}
+
+}
